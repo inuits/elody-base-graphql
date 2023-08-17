86cb14996d77daca0774238d5514b78d663cb558
Berend Vandenbussche <berend@inuits.eu>
2023-08-17 10:02:21 +0200

    Add log and path rewrite

--- sources/prom.ts
+++ sources/prom.ts
@@ -2,18 +2,22 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import { Express } from 'express';
 import fetch from 'node-fetch';
 
+const logReq = (proxyReq: any, req: any, res: any) => {
+  console.log(proxyReq);
+};
 
-const applyPromEndpoint = (
-  app: Express,
-  promUrl: string,
-) => {
+const applyPromEndpoint = (app: Express, promUrl: string) => {
   app.use(
     '/api/prom',
     createProxyMiddleware({
       target: promUrl,
       changeOrigin: true,
+      pathRewrite: {
+        '^/api/prom': '/',
+      },
+      onProxyReq: logReq,
     })
-  )
+  );
 };
 
 export default applyPromEndpoint;
