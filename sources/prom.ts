9763a19b7c0f1a42ecc6d76f28e884c006f9e9e1
siltn depuydt <siltn@inuits.eu>
2023-08-24 10:38:58 +0200

    remove loggers

--- sources/prom.ts
+++ sources/prom.ts
@@ -1,26 +1,17 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { Express } from 'express';
 
-const logReq = (proxyReq: any, req: any, res: any) => {
-  console.log('proxyReq', proxyReq);
-  console.log('req', req);
-  console.log('res', res);
-};
-
 const applyPromEndpoint = (app: Express, promUrl: string) => {
-  if (promUrl) {
-    app.use(
-      '/api/prom',
-      createProxyMiddleware({
-        target: promUrl,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/prom': '/api/v1',
-        },
-        onProxyReq: logReq,
-      })
-    );
-  }
+  app.use(
+    '/api/prom',
+    createProxyMiddleware({
+      target: promUrl,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/prom': '/api/v1',
+      },
+    })
+  );
 };
 
 export default applyPromEndpoint;
