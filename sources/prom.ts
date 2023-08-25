0a8ab1a0d909455ee6d1ed806e3fb66cb0cc3671
Berend Vandenbussche <berend@inuits.eu>
2023-08-25 11:58:53 +0200

    Only create proxy when promUrl is present

--- sources/prom.ts
+++ sources/prom.ts
@@ -2,16 +2,18 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import { Express } from 'express';
 
 const applyPromEndpoint = (app: Express, promUrl: string) => {
-  app.use(
-    '/api/prom',
-    createProxyMiddleware({
-      target: promUrl,
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/prom': '/api/v1',
-      },
-    })
-  );
+  if (promUrl) {
+    app.use(
+      '/api/prom',
+      createProxyMiddleware({
+        target: promUrl,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/prom': '/api/v1',
+        },
+      })
+    );
+  }
 };
 
 export default applyPromEndpoint;
