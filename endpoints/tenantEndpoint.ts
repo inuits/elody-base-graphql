bde9927956f439e893757db482a4cfa6d7abe5b5
Berend Vandenbussche <berend@inuits.eu>
2023-10-10 09:12:21 +0200

    Change 'tenant' endpoint to 'tenants'

--- endpoints/tenantEndpoint.ts
+++ endpoints/tenantEndpoint.ts
@@ -1,5 +1,5 @@
-import express, {Express} from "express";
-declare module "express-session" {
+import express, { Express } from 'express';
+declare module 'express-session' {
   interface SessionData {
     tenant: string | undefined;
   }
@@ -7,12 +7,12 @@ declare module "express-session" {
 export const applyTenantEndpoint = (app: Express) => {
   app.use(express.json());
 
-  app.get("/api/tenant", async (req, res) => {
+  app.get('/api/tenants', async (req, res) => {
     res.status(200).json(req.session.tenant || {});
   });
 
-  app.post("/api/tenant", async (req, res) => {
-   req.session.tenant = req.body.tenant;
-   res.status(200).end("tenant set");
+  app.post('/api/tenants', async (req, res) => {
+    req.session.tenant = req.body.tenant;
+    res.status(200).end('tenant set');
   });
-}
+};
