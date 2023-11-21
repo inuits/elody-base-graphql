0e61f53cc68692d128ab6ceeb294d4e0931e54b7
Eray Özcan <eray@inuits.eu>
2023-11-21 18:04:48 +0100

    Refs #119210 refactor upload flow

--- endpoints/uploadEndpoint.ts
+++ endpoints/uploadEndpoint.ts
@@ -1,159 +1,81 @@
-import { Express, Request, Response } from 'express';
-import { environment as env } from '../main';
+import fetch from 'node-fetch';
 import { addJwt } from './mediafilesEndpoint';
-import {
-  EntityInput,
-  Metadata,
-  MediaFileInput,
-  Entitytyping,
-} from '../../../generated-types/type-defs';
-import fetch, { Response as FetchResponse } from 'node-fetch';
-
-type UploadRequestData = {
-  body: any;
-  uri: string;
-};
-
-const filenamePlaceholder = 'filename_placeholder';
-const createEntityUriQueryParameters = `create_mediafile=1&mediafile_filename=${filenamePlaceholder}`;
+import { environment as env } from '../main';
+import { Express, Request, Response } from 'express';
 
 export const applyUploadEndpoint = (app: Express) => {
-  app.post(
-    `/api/upload/request-data`,
-    async (request: Request, response: Response) => {
-      try {
-        let body = '';
-        request.on('data', (chunk) => (body += chunk.toString()));
-        request.on('end', async () => {
-          const uploadRequestData = await getUploadRequestData(request, body);
-          response.end(JSON.stringify(uploadRequestData));
-        });
-      } catch (exception) {
-        response.status(500).end(String(exception));
-      }
-    }
-  );
-
-  app.post(`/api/upload`, async (request: Request, response: Response) => {
+  app.post('/api/upload/batch', async (request: Request, response: Response) => {
     try {
-      const uploadUrl = await getUploadUrl(request)
-        .then(async (urlResponse: FetchResponse) => {
-          if (!urlResponse.ok) throw urlResponse;
-          return await urlResponse.text();
-        })
-        .catch(async (UrlResponse: FetchResponse) =>
-          response.status(UrlResponse.status).end(await UrlResponse.text())
-        );
+      let csv = '';
+      request.on('data', (chunk) => (csv += chunk.toString()));
+      request.on('end', async () => {
+        const uploadUrls = (await __batchEntities(request, csv)).filter(uploadUrl => uploadUrl !== "");
+        response.end(JSON.stringify(uploadUrls));
+      });
+    } catch (exception) {
+      response.status(500).end(String(exception));
+    }
+  });
 
-      response.status(200).setHeader('Content-Type', 'application/json');
-      response.end(JSON.stringify({ url: uploadUrl }));
+  app.post('/api/upload/single', async (request: Request, response: Response) => {
+    try {
+      const uploadUrl = await __createMediafileForEntity(request);
+      response.end(JSON.stringify(uploadUrl));
     } catch (exception) {
       response.status(500).end(String(exception));
     }
   });
 };
 
-const getUploadRequestData = async (
-  request: Request,
-  body: string
-): Promise<UploadRequestData> => {
-
-    const response = await fetch(
-      `${env?.api.collectionApiUrl}/batch`,
-      {
-        method: 'POST',
-        body,
-        headers: {
-          'Content-Type': 'text/csv',
-          'Accept': 'text/uri-list',
-          Authorization: addJwt(undefined, request, undefined),
-        },
-      }
-    );
-
-    let result = '';
-    response.body.on('data', (chunk) => (result += chunk.toString()));
-    return new Promise((resolve) => {
-      response.body.on('end', async () => {
-        let jsonstr = `["${result.split('\n').join('","')}"]`;
-        const uploadRequestData = {
-          body: JSON.parse( jsonstr),
-          uri: `${env?.api.collectionApiUrl}/entities?${createEntityUriQueryParameters}`,
-        };
-        resolve(uploadRequestData);
-      });
-    });
-
-};
-const defaultEntityData = (entityToCreate: Entitytyping): UploadRequestData => {
-  const entityBody: EntityInput = {
-    id: `${filenamePlaceholder}`,
-    type: entityToCreate,
-    metadata: [
-      {
-        key: 'title',
-        value: `${filenamePlaceholder}`,
+const __batchEntities = async (request: Request, csv: string): Promise<string[]> => {
+  const response = await fetch(
+    `${env?.api.collectionApiUrl}/batch`,
+    {
+      headers: {
+        'Content-Type': 'text/csv',
+        Accept: 'text/uri-list',
+        Authorization: addJwt(undefined, request, undefined),
       },
-    ] as Metadata[],
-  };
-
-  return {
-    body: entityBody,
-    uri: `${env?.api.collectionApiUrl}/entities?${createEntityUriQueryParameters}`,
-  };
-};
-
-const defaultMediafileData = (): UploadRequestData => {
-  const mediafileBody: MediaFileInput = {
-    filename: `${filenamePlaceholder}`,
-    metadata: [
-      {
-        key: 'title',
-        value: `${filenamePlaceholder}`,
-      },
-    ] as Metadata[],
-  };
-
-  return {
-    body: mediafileBody as string,
-    uri: env?.api.collectionApiUrl + 'mediafiles',
-  };
-};
-
-const verifyUploadRequest = (request: Request) => {
-  const replacePlaceholders = (obj: any, newValue: string) => {
-    for (const key in obj) {
-      const value = obj[key];
-      if (value === filenamePlaceholder) obj[key] = newValue;
-      else if (typeof value === 'object') replacePlaceholders(value, newValue);
+      method: 'POST',
+      body: csv,
     }
-  };
-
-  const uploadRequestData = request.body as UploadRequestData;
-  const filename = request.query.filename as string;
-  replacePlaceholders(uploadRequestData.body, filename);
-  uploadRequestData.uri = uploadRequestData.uri.replace(
-    filenamePlaceholder,
-    filename
   );
+
+  let responseBody = '';
+  response.body.on('data', (chunk) => (responseBody += chunk.toString()));
+  return new Promise((resolve) => {
+    response.body.on('end', async () => {
+      const jsonParsableResult = `["${responseBody.split('\n').join('","')}"]`;
+      resolve(JSON.parse(jsonParsableResult));
+    });
+  });
 };
 
-const getUploadUrl = async (request: Request): Promise<FetchResponse> => {
-  const entityIdUri = request.query.entityIdUri || "";
-  return fetch(`${env?.api.collectionApiUrl}${entityIdUri}/mediafiles`, {
-    method: 'POST',
-    body: JSON.stringify({
-    filename: `${request.query.filename }`, metadata: [
-        {
-          key: 'title',
-          value: `${request.query.filename}`,
-        },
-      ],
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: addJwt(undefined, request, undefined),
-      Accept: 'text/uri-list',
-    },
+const __createMediafileForEntity = async (request: Request): Promise<string> => {
+  const response = await fetch(
+    `${env?.api.collectionApiUrl}/entities/${request.query.entityId}/mediafiles`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'text/uri-list',
+        Authorization: addJwt(undefined, request, undefined),
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        filename: `${request.query.filename }`,
+        metadata: [
+          {
+            key: 'title',
+            value: `${request.query.filename}`,
+          },
+        ],
+      }),
+    }
+  );
+
+  let responseBody = '';
+  response.body.on('data', (chunk) => (responseBody += chunk.toString()));
+  return new Promise((resolve) => {
+    response.body.on('end', async () => resolve(responseBody));
   });
 };
