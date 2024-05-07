67ac98ecd215da79744f4916593a0eea6d5e839c
Jonas Verschueren <jonasv@inuits.eu>
2024-05-07 15:01:34 +0200

    revert: Put slash at the back of the baseURL in transcode

--- sources/transcode.ts
+++ sources/transcode.ts
@@ -3,7 +3,7 @@ import {MediaFile, TranscodeType} from "../../../generated-types/type-defs";
 import {environment as env} from "../main";
 
 export class TranscodeService extends AuthRESTDataSource {
-    public baseURL = `${env?.api.transcodeService}`;
+    public baseURL = `${env?.api.transcodeService}/`;
 
     async generateTranscode(mediafiles: MediaFile[], transcodeType: TranscodeType, masterEntityId: string | undefined = undefined): Promise<any>{
         const mediafileObject = {'mediafiles': mediafiles}
@@ -12,7 +12,7 @@ export class TranscodeService extends AuthRESTDataSource {
 
     async downloadItemsInZip(body: any): Promise<any> {
         return await this.post(
-            `/transcode/zip`,
+            `transcode/zip`,
             {
                 body
             }
