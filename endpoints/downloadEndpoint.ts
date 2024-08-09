82fe232893c6de7f5cf1c8ed336e7815cb42d471
Jonas Verschueren <jonasv@inuits.eu>
2024-08-09 15:47:21 +0200

    feat: Finish download csv endpoint and link correctly with backend
    Refs: #127309

--- endpoints/downloadEndpoint.ts
+++ endpoints/downloadEndpoint.ts
@@ -1,34 +1,22 @@
-import fetch, { Response as FetchResponse } from 'node-fetch';
-import { addJwt } from './mediafilesEndpoint';
-import { environment as env } from '../main';
+import {AuthRESTDataSource, environment as env} from '../main';
 import { Express, Request, Response } from 'express';
 
 export const applyDownloadEndpoint = (app: Express) => {
     app.post(
-      `/api/download/csv`,
-      async (request: Request, response: Response) => {
-        try {
-            await fetch(
-                `${env?.api.collectionApiUrl}/entities${request.query.parentId}`,
-                {
-                    method: 'POST',
-                    body: JSON.stringify(request.query.relationType),
-                    headers: {
-                        Accept: 'text/csv',
-                        Authorization: addJwt(undefined, request, undefined),
-                    },
-                }
-            )
-                .then(async (urlResponse: FetchResponse) => {
-                    if (!urlResponse.ok) throw urlResponse;
-                    response.status(200).setHeader('Content-Type', 'text/csv');
-                    response.end(await urlResponse.text());
-                })
-                .catch(async (UrlResponse: FetchResponse) =>
-                    response.status(UrlResponse.status).end(await UrlResponse.text())
+        `/api/download/csv`,
+        async (request: Request, response: Response) => {
+            try {
+                const datasource = new AuthRESTDataSource({ session: request.session });
+                const result = await datasource.post(
+                    `${env?.api.collectionApiUrl}/entities/${request.body.parentId}/order`,
+                    {
+                        method: 'GET',
+                    }
                 );
-        } catch (exception) {
-            response.status(500).end(String(exception));
+                response.status(200).setHeader('Content-Type', 'text/csv');
+                response.end(result);
+            } catch (exception) {
+                response.status(500).end(String(exception));
         }
     });
 };
