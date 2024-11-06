cd73167b323ba977613fb904db65b0cbc22d6dc9
Berend Vandenbussche <berend@inuits.eu>
2024-11-06 13:29:38 +0100

    fix: Serve frontend when none of the special cases have been met
    Refs #105106

--- endpoints/frontendEndpoint.ts
+++ endpoints/frontendEndpoint.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 
+export const serveFrontend = (req: Request, res: Response): void => {
+  const __dirname: string = path.resolve();
+  const frontendPath: string = path.join(__dirname, 'dashboard/dist');
+  res.sendFile(path.resolve(frontendPath, 'index.html'));
+};
+
 export const serveFrontendThroughExpress = (app: any) => {
   const __dirname: string = path.resolve();
   const frontendPath: string = path.join(__dirname, 'dashboard/dist');
@@ -8,6 +14,6 @@ export const serveFrontendThroughExpress = (app: any) => {
   app.use(express.static(frontendPath));
 
   app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(frontendPath, 'index.html'));
+    serveFrontend(req, res);
   });
 };
