8222371b526d62a772c3e0891d164c223f322f94
Berend Vandenbussche <berend@inuits.eu>
2024-12-10 10:11:42 +0100

    Fix: fix mongodb connection string create function

--- sources/mongo.ts
+++ sources/mongo.ts
@@ -1,19 +1,24 @@
 import { Environment } from '../environment';
 
+const quotePlus = (str: string): string =>
+  encodeURIComponent(str).replace(/%20/g, '+');
+
 export const createMongoConnectionString = (appConfig: Environment): string => {
   const mongoConfig = appConfig.db.mongodb;
   let connectionString: string = 'mongodb://';
   if (mongoConfig.username && mongoConfig.password) {
-    connectionString += `${mongoConfig.username}:${mongoConfig.password}`;
+    connectionString += `${quotePlus(mongoConfig.username)}:${quotePlus(
+      mongoConfig.password
+    )}`;
   }
   const mongoHostsArray: string[] = mongoConfig.hostname.split(',');
-  mongoHostsArray.forEach((hostname) => {
+  mongoHostsArray.forEach((hostname, index) => {
     if (mongoConfig.port) {
       connectionString += `${hostname}:${mongoConfig.port}`;
     } else {
       connectionString += `${hostname}`;
     }
-    if (mongoHostsArray.length < 1) {
+    if (index < mongoHostsArray.length - 1) {
       connectionString += ',';
     }
   });
