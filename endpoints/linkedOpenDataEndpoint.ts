13c2fc9b630c7e11e9c8864cf66c4933567561c3
Jonas Verschueren <jonasv@inuits.eu>
2025-04-28 16:24:44 +0200

    fix: Split up serve and not serve frontend flows in if else structure
    Refs: #140906

--- endpoints/linkedOpenDataEndpoint.ts
+++ endpoints/linkedOpenDataEndpoint.ts
@@ -33,7 +33,6 @@ const returnEntityInFormat = async (
         const status = exception.extensions.response.status || 500;
         const statusText = exception.extensions.response.statusText || String(exception);
         res.status(status).end(statusText);
-        res.end(JSON.stringify(exception));
     }
 };
 
@@ -59,30 +58,29 @@ const getEntityIdFromPath = (path: string): string => {
     return path.match(regex)![1];
 };
 
-const shouldServeFrontend = (format: string | undefined, req: Request, res: Response): void => {
-    if (!format || acceptHeadersForFormat[format] === undefined ) {
-        console.log("Serving frontend");
-        serveFrontend(req, res);
-    }
+const shouldServeFrontend = (format: string | undefined): boolean => {
+    return (!format || acceptHeadersForFormat[format] === undefined);
 };
 
 export const applyLinkedOpenDataEndpoint = (app: Express) => {
     app.get("/*", async (req: Request, res: Response): Promise<void> => {
         try {
             const format = req.headers.accept;
-            shouldServeFrontend(format, req, res);
-            console.log("NOT serving frontend");
-
-            const entityId = getEntityIdFromPath(req.url);
-            if (await isEntityPresent(entityId, req)) await returnEntityInFormat(entityId, format!, req, res)
-            else res.status(404).end("Entity not found!");
+            if (shouldServeFrontend(format)) {
+                console.log("Serving frontend");
+                serveFrontend(req, res);
+                res.status(200).setHeader('Content-Type', "application/json").end("Serving frontend");
+            } else {
+                console.log("NOT serving frontend");
+                const entityId = getEntityIdFromPath(req.url);
+                if (await isEntityPresent(entityId, req)) await returnEntityInFormat(entityId, format!, req, res)
+                else res.status(404).end("Entity not found!");
+            }
         } catch (exception: any) {
             const status = exception.extensions.response.status || 500;
             const statusText = exception.extensions.response.statusText || String(exception);
             res.status(status).end(statusText);
-            res.end(JSON.stringify(exception));
         }
 
     });
-
 }
