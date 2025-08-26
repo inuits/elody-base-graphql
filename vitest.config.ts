e307c50756fbf8dc365a6dae691f7cc50d72d899
boriss <boriss@inuits.eu>
2025-08-26 12:54:33 +0200

    temp: test job v35

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,6 @@ import path from "path";
 export default defineConfig({
   test: {
     setupFiles: [path.resolve(__dirname, "vitest.setup.ts")],
-    alias: {
-      "@/generated-types/type-defs": "@/__mock__/types",
-    },
     environment: "node",
   },
 });
