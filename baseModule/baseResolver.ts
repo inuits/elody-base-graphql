16eb1204ba16c6b2fbbdc2345a2c76154bd018c8
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-10-21 14:51:13 +0200

    feat(tracing): refactor

--- baseModule/baseResolver.ts
+++ baseModule/baseResolver.ts
@@ -111,7 +111,7 @@ import {
   AdvancedFilterInputType,
   LookupInputType,
   AutocompleteSelectionOptions,
-  // MapFeatureMetadata,
+  MapFeatureMetadata,
 } from '../../../generated-types/type-defs';
 import { ContextValue } from '../types';
 import { baseFields } from '../sources/forms';
@@ -1163,9 +1163,9 @@ export const baseResolver: Resolvers<ContextValue> = {
     geoJsonFeature: async (parent: unknown, {}, { dataSources }) => {
       return parent as GeoJsonFeature;
     },
-    // mapFeatureMetadata: async (parent: unknown, {}, { dataSources }) => {
-    //   return parent as MapFeatureMetadata;
-    // },
+    mapFeatureMetadata: async (parent: unknown, {}, { dataSources }) => {
+      return parent as MapFeatureMetadata;
+    },
     config: async (_source, { input }, { dataSources }) => {
       return input as ConfigItem[];
     },
@@ -2407,9 +2407,9 @@ export const baseResolver: Resolvers<ContextValue> = {
       return parent.facets || [];
     },
   },
-  // MapFeatureMetadata: {
-  //   metaData: async (parent: unknown, {}, { dataSources }) => {
-  //     return parent as PanelMetaData;
-  //   },
-  // },
+  MapFeatureMetadata: {
+    metaData: async (parent: unknown, {}, { dataSources }) => {
+      return parent as PanelMetaData;
+    },
+  },
 };
