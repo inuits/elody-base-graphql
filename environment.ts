0b04bdf7031f2953918cf3ffaa1520c8dec01efe
Jonas Verschueren <jonasv@inuits.eu>
2025-10-28 10:28:52 +0100

    fix: Make clientKeyFormat optional in config

--- environment.ts
+++ environment.ts
@@ -49,7 +49,7 @@ export interface Environment {
     simpleSearch?: {
       itemTypes?: Entitytyping[];
       simpleSearchMetadataKey: string[];
-      clientKeyFormat: string[]
+      clientKeyFormat?: string[]
     };
     ipWhiteListing?: {
       whiteListedIpAddresses: string[];
