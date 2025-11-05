f208d9200af3c0444de4f9ebed3900b53ed5ff7b
Berend Vandenbussche <berend@inuits.eu>
2025-11-05 14:09:40 +0100

    Chore: Add whitelisted IP

--- sources/storage.ts
+++ sources/storage.ts
@@ -1,4 +1,3 @@
-import FormData from 'form-data';
 import { AuthRESTDataSource } from '../auth/AuthRESTDataSource';
 import { getCurrentEnvironment } from '../environment';
 import { Environment } from '../types/environmentTypes';
@@ -6,21 +5,4 @@ import { Environment } from '../types/environmentTypes';
 export class StorageAPI extends AuthRESTDataSource {
   env: Environment = getCurrentEnvironment();
   public baseURL = `${this.env.api.storageApiUrl}`;
-
-  async uploadFile(id: String, file: any): Promise<any> {
-    const form = new FormData();
-    const { createReadStream, filename, mimetype, encoding, knownLength } =
-      await file;
-    form.append('file', createReadStream(), {
-      filename: filename,
-      contentType: mimetype,
-      knownLength: knownLength,
-    });
-    const formHeaders = form.getHeaders();
-    const upload = await this.post(`upload?id=${id}`, {
-      body: { form },
-      headers: formHeaders,
-    });
-    return upload;
-  }
 }
